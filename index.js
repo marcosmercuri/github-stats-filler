d8052bfb15d7b2e3ba85f613782ac4ac2ad5309c
Marcos Mercuri <marcosmercuri@gmail.com>
2020-08-20 23:24:10 +0200

    Add lambda function that reads env config vars and fills past week

---
+++ index.js
@@ -0,0 +1,20 @@
+const fill = require('./src/fill')
+
+exports.handler = async (event, context) => {
+  const today = new Date()
+  const config = {
+    auth: {
+      username: process.env.AUTH_USERNAME,
+      password: process.env.AUTH_PASSWORD
+    },
+    repoUrl: process.env.REPO_URL,
+    author: {
+      name: process.env.AUTHOR_NAME,
+      email: process.env.AUTHOR_EMAIL
+    },
+    dir: process.env.DIR,
+    maxNumberOfCommits: 80,
+    endDate: today
+  }
+  return await fill(config)
+}
